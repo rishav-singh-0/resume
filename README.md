f248296c188bacf82ff0e47b51d06910f2c2b93f
Rishav Singh <70544726+rishav-singh-0@users.noreply.github.com>
2021-09-16 19:28:40 +0530

    Converted to English and somewhat personalized

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 
-open source resume(html+js+css)
+## Open source resume(html+css+js)
+I applied for a resume by myself, and I can change it to myself if I like it;
+The code written is not standard enough, and those who are interested can assist in modifying it.
 
-<h2>开源简历（open source resume）</h2>
-自己应聘做了一个简历,喜欢的可以去改成自己；
-code写的也不够标准，有兴趣的可以协助修改下。
+Some features:
 
-一些特色：
-<ul>
-  <li>响应式布局</li>
-  <li>局部打印</li>
-  <li>语音操作</li>
-</ul>
+- Responsive layout
+- Partial printing
+- Voice operation
 
 
--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
-/*执行事件*/
+/*Execution event*/
 $(function() {
 
-    //打印
+    //Print
     $('.button_print').click(function(){
         $('.main').print({
         addGlobalStyles : true,
@@ -11,7 +11,7 @@ $(function() {
 
     //animate avatar info
     $('.avatar_pic,.tel_me').click(function(){
-        $('.tip_avatar_bg').show().transition({ 
+        $('.tip_avatar_bg').show().transition({
             opacity: 100,
             top: '0',
             left: '0',
@@ -42,9 +42,9 @@ $(function() {
             rotate: '180deg'
         },300);
         $('body').removeClass('of');
-        $('.tip_avatar').transition({ 
-            scale: 0.8,            
-        },500,'easeOutQuart').transition({ 
+        $('.tip_avatar').transition({
+            scale: 0.8,
+        },500,'easeOutQuart').transition({
             y: -500,
             opacity: 0
         },function(){
@@ -71,7 +71,7 @@ $(function() {
         $(this).parent().removeClass('focus');
         $('.info_content').removeClass('over')
         $('.info_content .desc').removeClass('curr');
-    }); 
+    });
 
     //banner
     $('.carousel').carousel({
@@ -99,13 +99,13 @@ $(function() {
         $('.carousel').addClass('slide');
     }
 
-    //product carousel (以0为开始)
+    //product carousel (Start with 0)
     $('.product_1').click(function(){
         del_slide();
         $('.carousel').carousel(0);
         add_slide();
         open_layout();
-    }); 
+    });
 
     $('.product_2').click(function(){
         del_slide();
@@ -164,7 +164,7 @@ $(function() {
         scrollPos = $(this).scrollTop();
         $('.logo').css({
             opacity: 1-(scrollPos/300),
-            top: 50+(scrollPos/8)+'%'       
+            top: 50+(scrollPos/8)+'%'
         });
         $('.banner_bg').css({
             opacity: 1-(scrollPos/300)
@@ -175,9 +175,9 @@ $(function() {
     });
 
     //close sound help
-    $('.close_help').click(function(){
-        soundhelpClose();
-    });
+     $('.close_help').click(function(){
+         soundhelpClose();
+     });
 
 });
 
@@ -230,10 +230,10 @@ if (annyang) {
 
   };
 
-  // Add our commands to annyang
-  annyang.addCommands(commands);
+   // Add our commands to annyang
+   annyang.addCommands(commands);
 
-  // Start listening.
-  annyang.start();
+   // Start listening.
+   annyang.start();
 
 }
